511fde37b439e92f61062d587998d91c7f0587ca
Adrian Simionescu <simionescuadrian@yahoo.com>
2022-02-17 22:11:12 +0400

    partial solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'bookmarks/index'
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/create'
+  get 'lists/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [ :index, :new, :create, :show ] do
+    resources :bookmarks, only: [:index, :new, :create, ]
+  end
+  delete 'bookmark/:id', to: 'bookmarks#destroy', as: :bookmark
 end
